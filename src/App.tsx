0e30584d07b109c73353019ec503ca870f23a5a0
Self Improvement Lover <aotcaplevi4@gmail.com>
2024-10-31 11:40:33 +0000

    Tested Streaming Metadata Provider API and set up:
    - babel config
    - jest config
    - allowing for tests to use enviroment variables in vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import { TMDBApiStreamingContentMetadataProvider } from './providers/tmdb-api-streaming-content-metadata-provider';
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const provider = new TMDBApiStreamingContentMetadataProvider();
+  async function onClick() {
+    const provider = new TMDBApiStreamingContentMetadataProvider();
+    const consoleResponse =
+      await provider.getStreamingContentMetadata('lord of rings');
+    console.log(consoleResponse);
+  }
 
-  return <></>;
+  return (
+    <>
+      <button onClick={onClick}>anime metadata test</button>
+    </>
+  );
 }
-
-export default App;
---
+++ src/providers/tmdb-api-streaming-content-metadata-provider.ts
@@ -0,0 +1,104 @@
+import {
+  StreamingContentMetadata,
+  StreamingContentMetadataProvider,
+} from './streaming-content-metadata-provider';
+import { HTTPError } from './http-error';
+
+export class TMDBApiStreamingContentMetadataProvider extends StreamingContentMetadataProvider {
+  async getStreamingContentMetadata(
+    searchInput: string,
+  ): Promise<StreamingContentMetadata[]> {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/multi?query=${searchInput}`,
+      {
+        headers: {
+          authorization: `Bearer ${import.meta.env.VITE_READ_ACCESS_TOKEN}`,
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new HTTPError(
+        'GET',
+        response.url,
+        response.status,
+        await response.text(),
+      );
+    }
+
+    const body: TMDBMultiStreamingContentMetadataResponse = await response.json();
+    const moviesAndTVshows: ResultsForMoviesAndTVseries[] = body.results.filter(
+      (x) => x.media_type !== 'person',
+    );
+    
+    const streamingContentMetadataArray = moviesAndTVshows.map((x) => {
+      const streamingContentMetadata = {
+        coverImage: x.poster_path || '../assets/cover-unavailable.webp',
+        title: (x.title || x.name) as string,
+        description: x.overview,
+        releaseDate: (x.release_date || x.first_air_date) as string,
+        genreIds: x.genre_ids,
+        rating: x.vote_average,
+        mediaType: x.media_type,
+      };
+
+      return streamingContentMetadata;
+    });
+
+    return streamingContentMetadataArray;
+  }
+}
+
+
+interface TMDBMultiStreamingContentMetadataResponse {
+  page: number;
+  results: MultiSearchResults[];
+  total_pages: number;
+  total_results: number;
+
+}
+
+interface MultiSearchResults {
+  backdrop_path?: string;
+  id: number;
+  name?: string;
+  original_name?: string;
+  overview: string;
+  poster_path?: string;
+  media_type: string;
+  adult: boolean;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  first_air_date?: string;
+  vote_average: number;
+  vote_count: number;
+  origin_country?: string[];
+  title?: string;
+  original_title?: string;
+  release_date?: string;
+  video?: boolean;
+}
+
+
+interface ResultsForMoviesAndTVseries {
+  adult: boolean;
+  backdrop_path?: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title?: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string;
+  first_air_date?: string;
+  name?: string;
+  release_date?: string;
+  title?: string;
+  video?: boolean;
+  vote_average: number;
+  vote_count: number;
+  media_type: string
+}
+
+ 
